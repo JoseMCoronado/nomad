aade7a8f7304261cee30b1326fd78d951ec9c4b4
JoseMCoronado <j.coronado.personal@gmail.com>
2022-09-14 16:42:35 -0700

    [RM] Default Django admin functionality

--- nomad/urls.py
+++ nomad/urls.py
@@ -13,14 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path, include
 
-admin.site.site_header = "Nomad Admin"
-admin.site.index_title = "Access Rights"
-
 urlpatterns = [
-    path("admin/", admin.site.urls),
     path("__debug__/", include("debug_toolbar.urls")),
     path("tracker/", include("tracker.urls")),
 ]
